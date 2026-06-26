7dc50f3d6a3d0f51cf42a1f08ed4c3da6ae48bb0
Gabriel <guedoladev@gmail.com>
2026-06-25 21:24:21 -0300

    feat: tela de conteudo da noticia

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq4SUximV4NM8SJTs7oaMNHFM2p4KdeBHJQwDBFf4Bug&s=10",
+    "title": "Mono Rato",
+    "description": "Titititititi"
+  },
+  {
+    "id": "2",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQepHE1_jpIh2o1_Jqja38NJlMf2P3A8FfAxWgorSqyKw&s=10",
+    "title": "Mono Kaisa",
+    "description": "Pelo vazio"
+  },
+  {
+    "id": "3",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-KNpFi3ZTDj3D7NZ4MFxjweXN4y4_yPsrr8-w9rNH8A&s=10",
+    "title": "Mono Jinx",
+    "description": "Jinx, é de Jinx"
+  },
+  {
+    "id": "4",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfy1eeJBVj4tSP7XFCXp7c0pM3ihX84ydHe0yvGrgtEg&s=10",
+    "title": "Mono Ezreal",
+    "description": "Seu lugar é no museu"
+  },
+]
